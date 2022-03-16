b40b93c097693f57dc525e185d487f285346aae6
Abdullah Dinler <71527576+abdullahdinler@users.noreply.github.com>
2022-03-16 15:35:21 +0300

    Update Form1.cs

--- LoginTasarim/Form1.cs
+++ LoginTasarim/Form1.cs
@@ -16,15 +16,5 @@ namespace LoginTasarim
         {
             InitializeComponent();
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void groupBox1_Enter(object sender, EventArgs e)
-        {
-
-        }
     }
 }
